4ed3dd9344214365dde020e78b1acb7aafcceb36
Muhammad Nuriansyah Malik Nugraha <nuriansyah09@gmail.com>
2023-04-07 06:51:26 +0700

    chore: update host database

--- cmd/config/database.go
+++ cmd/config/database.go
@@ -19,13 +19,13 @@ func NewInitializedDatabase(config Config) (*sql.DB, error) {
 func NewPostgresSQL(configuration Config) (*sql.DB, error) {
 	username := configuration.Get("DB_USERNAME")
 	password := configuration.Get("DB_PASSWORD")
-	host := fmt.Sprintf("/cloudsql/%s", configuration.Get("DB_HOST"))
+	connectionName := configuration.Get("DB_HOST") // Menggunakan nama koneksi Cloud SQL langsung
 	port := configuration.Get("DB_PORT")
 	database := configuration.Get("DB_DATABASE")
 	sslMode := configuration.Get("DB_SSL_MODE")
 
-	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s", username, password, host, port, database, sslMode)
-	db, err := sql.Open("cloudsqlpostgres", dsn)
+	dsn := fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, connectionName, database, port, sslMode)
+	db, err := sql.Open("cloudsqlpostgres", dsn) // Gunakan "cloudsqlpostgres" sebagai nama driver
 	if err != nil {
 		return nil, err
 	}
