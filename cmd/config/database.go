364c318a0a1b69a541e73d8c6f9256c3da6a68b8
Muhammad Nuriansyah Malik Nugraha <nuriansyah09@gmail.com>
2023-04-07 08:37:24 +0700

    chore: update host database

--- cmd/config/database.go
+++ cmd/config/database.go
@@ -1,10 +1,17 @@
 package config
 
 import (
+	"cloud.google.com/go/cloudsqlconn"
 	_ "cloud.google.com/go/cloudsqlconn"
+	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"net"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -17,24 +24,48 @@ func NewInitializedDatabase(config Config) (*sql.DB, error) {
 
 	return db, nil
 }
-func NewPostgresSQL(configuration Config) (*sql.DB, error) {
-	username := configuration.Get("DB_USERNAME")
-	password := configuration.Get("DB_PASSWORD")
-	host := configuration.Get("DB_HOST")
-	port := configuration.Get("DB_PORT")
-	database := configuration.Get("DB_DATABASE")
-	sslMode := configuration.Get("DB_SSL_MODE")
+func NewPostgresSQL(conn Config) (*sql.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_connector.go: %s environment variable not set.\n", k)
+		}
+		return v
+	}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep passwords and other secrets safe.
+	var (
+		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		usePrivate             = os.Getenv("PRIVATE_IP")
+	)
 
-	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s", username, password, host, port, database, sslMode)
-	db, err := sql.Open(configuration.Get("DB_CONNECTION"), dsn)
+	dsn := fmt.Sprintf("user=%s password=%s database=%s", dbUser, dbPwd, dbName)
+	config, err := pgx.ParseConfig(dsn)
 	if err != nil {
 		return nil, err
 	}
-
-	err = db.Ping()
+	var opts []cloudsqlconn.Option
+	if usePrivate != "" {
+		opts = append(opts, cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()))
+	}
+	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
 	if err != nil {
 		return nil, err
 	}
-
-	return db, nil
+	// Use the Cloud SQL connector to handle connecting to the instance.
+	// This approach does *NOT* require the Cloud SQL proxy.
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName)
+	}
+	dbURI := stdlib.RegisterConnConfig(config)
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+	return dbPool, nil
 }
